5f507bb36ffea902a0dece971eb35f64b4603d4f
flolu <loflude@gmail.com>
2022-12-04 20:57:34 +0100

    cli server ping pong

--- cli.ts
+++ cli.ts
@@ -1,11 +1,35 @@
+import readline from 'readline'
 import WebSocket from 'ws'
 
-const ws = new WebSocket('ws://localhost:8080')
+function setupKeyListener(handler: (key: string) => void) {
+  readline.emitKeypressEvents(process.stdin)
+  process.stdin.setRawMode(true)
 
-ws.on('open', () => {
-  ws.send('something')
-})
+  process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+      process.exit()
+    } else {
+      handler(str)
+    }
+  })
+}
 
-ws.on('message', data => {
-  console.log('received: %s', data)
-})
+async function main() {
+  const ws = new WebSocket('ws://localhost:8080')
+
+  ws.on('open', () => {
+    ws.send('ping')
+
+    setupKeyListener(() => {
+      ws.send('ping')
+    })
+  })
+
+  ws.on('message', data => {
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+    process.stdout.write(data.toString())
+  })
+}
+
+main()
--- server.ts
+++ server.ts
@@ -3,9 +3,7 @@ import {WebSocketServer} from 'ws'
 const wss = new WebSocketServer({port: 8080})
 
 wss.on('connection', ws => {
-  ws.on('message', data => {
-    console.log('received: %s', data)
+  ws.on('message', () => {
+    ws.send(Math.random())
   })
-
-  ws.send('something')
 })
